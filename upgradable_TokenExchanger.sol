2a19e00884844a2f9bb82d7d20e2aedbb781461d
hapsody <hapsody@gmail.com>
2018-12-26 17:56:11 +0900

    [v0.2.3] Adding code SafeMath.sol

--- upgradable_TokenExchanger.sol
+++ upgradable_TokenExchanger.sol
@@ -1,4 +1,4 @@
-// contract version 0.2.2
+// contract version 0.2.3
 // storage 컨트랙을 추가해서 TokenExchanger와 proxy 변수 slot 구조 동일하게 맞춤
 // owner외에 init 시도시 실패 확인
 // 이더 입금 & 출금 정상 확인
@@ -7,10 +7,58 @@
 // 코인 입금 & 출금 정상 확인
 // 코인 잔액보다 많이 출금 시도시 실패 확인
 
-
 pragma solidity ^0.4.25;
 
-import "./SafeMath.sol";
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
 
 contract Ownable {
     address internal owner;
@@ -434,7 +482,7 @@ contract NemodaxStorage is Ownable{
 contract ProxyNemodax is NemodaxStorage  {
 
     address private implementation;
-    event Upgraded(address newContract);
+    event Upgraded(address indexed newContract);
 
     function upgrade(address _addr) external {
         require(implementation != _addr);
